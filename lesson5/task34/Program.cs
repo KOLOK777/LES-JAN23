ba58d0fc3e99c7e107e2f5b899f0783d36fa9910
KOLOK777 <kolevg3@gmail.com>
2023-01-26 19:23:20 +0300

    финальная версия

--- lesson5/task34/Program.cs
+++ lesson5/task34/Program.cs
@@ -6,22 +6,30 @@ Console.WriteLine("Введите количесто элементов масс
 int len; 
 while (!Int32.TryParse(Console.ReadLine(), out len) || len <= 0)
     Console.WriteLine("Введены некорретные данные. Введите количесто элементов массива (целое положительное число): ");
-int[] mass = new int [len];
-int i;
-Console.Write("Cформирован массив:");
-for (i=0; i < len; i++){
-    mass[i] = new Random().Next(100,1000);
-    Console.Write($" {mass[i]} ");
-    }
-Console.WriteLine();
-int SUM = 0;
+if (len != 1)
+    {
+   int[] mass = new int [len];
+    int i;
+    Console.Write("Cформирован массив:");
+    for (i=0; i < len; i++){
+        mass[i] = new Random().Next(100,1000);
+        Console.Write($" {mass[i]} ");
+        }
+    Console.WriteLine();
+    int count = 0;
 
-void CHET(){
-    for (i = 0; i < len; i++){
-        if (mass[i] % 2 == 0) SUM++;
-    }
-    Console.WriteLine($"Количество четных чисел в массиве: {SUM}");
-    return;
-    }
+    void CHET(){
+        for (i = 0; i < len; i++){
+            if (mass[i] % 2 == 0) count++;
+            }
+        Console.WriteLine($"Количество четных чисел в массиве: {count} шт.");
+        return;
+        }
 
-CHET();
+    CHET();
+    }
+else 
+    if (new Random().Next(100,1000) % 2 == 0)
+        Console.WriteLine("Четных чисел - 1 шт");
+    else
+        Console.WriteLine("Четных чисел нет.");

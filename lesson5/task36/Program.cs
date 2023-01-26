b06a126f18e8b5225deb768f0d20dac60b0e2035
KOLOK777 <kolevg3@gmail.com>
2023-01-26 14:23:12 +0300

    финальная версия

--- lesson5/task36/Program.cs
+++ lesson5/task36/Program.cs
@@ -3,3 +3,27 @@
 // [3, 7, 23, 12] -> 19
 
 //[-4, -6, 89, 6] -> 0
+
+Console.WriteLine("Введите количесто элементов массива (целое положительное число): "); 
+int len; 
+while (!Int32.TryParse(Console.ReadLine(), out len) || len <= 0)
+    Console.WriteLine("Введены некорретные данные. Введите количесто элементов массива (целое положительное число): ");
+int[] mass = new int [len];
+int i;
+Console.Write("Cформирован массив:");
+for (i=0; i < len; i++){
+    mass[i] = new Random().Next();
+    Console.Write($" {mass[i]} ");
+    }
+Console.WriteLine();
+int SUM = 0;
+
+void CHET(){
+    for (i = 1; i < len; i = i + 2){
+        SUM = SUM + mass[i];
+    }
+    Console.WriteLine($"Cумма элементов массива, стоящих на нечётных позициях: {SUM}");
+    return;
+    }
+
+CHET();

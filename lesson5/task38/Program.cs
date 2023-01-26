857c83674f776f2b153fed3269f4f9ebc3e23006
KOLOK777 <kolevg3@gmail.com>
2023-01-26 19:00:13 +0300

    финальная версия

--- lesson5/task38/Program.cs
+++ lesson5/task38/Program.cs
@@ -1,16 +1,16 @@
 ﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
-
 // [3 7 22 2 78] -> 76
 
-Console.WriteLine("Введите количесто элементов массива (целое положительное число): "); 
+Console.WriteLine("Введите количесто элементов массива (целое число - больше 1): "); 
 int len; 
-while (!Int32.TryParse(Console.ReadLine(), out len) || len <= 0)
+while (!Int32.TryParse(Console.ReadLine(), out len) || len <= 1)
     Console.WriteLine("Введены некорретные данные. Введите количесто элементов массива (целое положительное число): ");
 int[] mass = new int [len];
 int i;
 Console.Write("Cформирован массив:");
-for (i=0; i < len; i++){
-    mass[i] = new Random().Next();
+for (i = 0; i < len; i++)
+    {
+    mass[i] = new Random().Next(0, 10);
     Console.Write($" {mass[i]} ");
     }
 
@@ -21,13 +21,13 @@ void RAZN()
     {
     max = mass[0];
     min = mass[0];
-
-    for (i = 1; i < len; i = i++)
+    Console.WriteLine();
+    for (i = 1; i < len; i++)
         {
         if (mass[i] > max) max = mass[i];
         if (mass[i] < min) min = mass[i];
         }
-    Console.WriteLine($"Разница между максимальным и минимальным элементами массива ({max} и {min}): {max - min}");
+    Console.WriteLine($"Разница между максимальным и минимальным элементами массива (макс: {max} и мин: {min}): '{max - min}'");
     return;
     }
     

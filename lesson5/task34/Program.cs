85cc6e4673e4abc41f765fd315f2e8ac859142a6
KOLOK777 <kolevg3@gmail.com>
2023-01-26 14:09:11 +0300

    финальная версия

--- lesson5/task34/Program.cs
+++ lesson5/task34/Program.cs
@@ -4,23 +4,23 @@
 
 Console.WriteLine("Введите количесто элементов массива (целое положительное число): "); 
 int len; 
-while (!Int32.TryParse(Console.ReadLine(), out len) || len > 0);
+while (!Int32.TryParse(Console.ReadLine(), out len) || len <= 0)
     Console.WriteLine("Введены некорретные данные. Введите количесто элементов массива (целое положительное число): ");
 int[] mass = new int [len];
 int i;
-Console.Write("Массив: ");
+Console.Write("Cформирован массив:");
 for (i=0; i < len; i++){
     mass[i] = new Random().Next(100,1000);
     Console.Write($" {mass[i]} ");
     }
-
+Console.WriteLine();
 int SUM = 0;
 
 void CHET(){
     for (i = 0; i < len; i++){
-        if (mass[i] % 2 == 0) SUM = SUM + mass[i];
+        if (mass[i] % 2 == 0) SUM++;
     }
-    Console.WriteLine($"Сумма четных чисел в массиве: {SUM}");
+    Console.WriteLine($"Количество четных чисел в массиве: {SUM}");
     return;
     }
 

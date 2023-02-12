47b5dff3cd7a82fa30549b97384d958a3f2a8e6f
KOLOK777 <kolevg3@gmail.com>
2023-02-12 18:30:02 +0300

    финальная версия

--- lesson7/task52/Program.cs
+++ lesson7/task52/Program.cs
@@ -14,26 +14,47 @@ int n;
 while (!Int32.TryParse(Console.ReadLine(), out n) || n <= 0)
     Console.WriteLine("Введены некорретные данные. Введите число столбцов массива (целое положительное число) 'n': ");
 
-int[,] array = new int [m,n];
-void FillArray()
-{
-    double[] AVG = new double [n];
-    for (int i = 0; i < m; i++)
-    {
-        for (int j = 0; j < n; j++)
-            {
-            array[i,j] = new Random().Next(0,5);
-            Console.Write($"{array[i,j]}  ");
-            if (i==0) AVG[j] = array[i,j];
-            else AVG[j] = AVG[j-1] + array[i,j];
-            }
-        Console.WriteLine();
-    for (int k = 0; k < n; k++){
-        Console.Write($"{AVG[k] = AVG[k] / m}; ");
-    return;
+int[,] GenerateArray(int m, int n){
+    int[,] array = new int[m, n];
+    Random random = new Random();
+    for (int i = 0; i < array.GetLength(0); i++) {
+        for (int j = 0; j < array.GetLength(1); j++) {
+            array[i,j] = random.Next();
+        }
     }
-    }
-return;
+    return array;
 }
 
-FillArray();
+void PrintArray(int[,] array){
+    for (int i = 0; i < array.GetLength(0); i++) {
+        for (int j = 0; j < array.GetLength(1); j++) {
+            Console.Write($"{array[i,j]}  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void PrintSingleArray(int[] average){
+    for (int i = 0; i < average.GetLength(0); i++) {
+        Console.Write($"{average[i]}  ");
+        }
+    }
+
+int[] SumColumn(int[,] array){
+    int[] average = new int[n];
+    int sum = 0;
+    for (int j = 0; j < array.GetLength(1); j++) {
+        sum = array[0,j];
+        for (int i = 1; i < array.GetLength(0); i++) {
+            sum = sum + array[i,j];
+            }
+        average[j] = sum / m;
+        }
+    return average;
+}
+
+var array = GenerateArray(m, n);
+PrintArray(array);
+Console.WriteLine("Среднее арифметическое каждого cтолбца: ");
+var average = SumColumn(array);
+PrintSingleArray(average); 

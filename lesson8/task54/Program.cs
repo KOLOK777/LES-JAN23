fad52de0ce32be352afb8099f74c45a49c875522
KOLOK777 <kolevg3@gmail.com>
2023-02-15 16:02:55 +0300

    финальная версия2

--- lesson8/task54/Program.cs
+++ lesson8/task54/Program.cs
@@ -22,7 +22,7 @@ int[,] GenerateArray(int m, int n){
     Random random = new Random();
     for (int i = 0; i < array.GetLength(0); i++) {
         for (int j = 0; j < array.GetLength(1); j++) {
-            array[i,j] = random.Next(1, 10);
+            array[i,j] = random.Next();
         }
     }
     return array;

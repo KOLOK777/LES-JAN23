b990cb9f310b044cced3367b677ac3a59b9e11dc
KOLOK777 <kolevg3@gmail.com>
2023-02-16 05:05:00 +0300

    обертка

--- lesson8/task62/Program.cs
+++ lesson8/task62/Program.cs
@@ -5,3 +5,37 @@
 // 11 16 15 06
 // 10 09 08 07
 
+Console.WriteLine("Введите число строк/столбцов массива (целое положительное число) 'm': "); 
+int m; 
+while (!Int32.TryParse(Console.ReadLine(), out m) || m <= 0)
+    Console.WriteLine("Введены некорретные данные. Введите число строк/столбцов массива (целое положительное число) 'm': ");
+int n = m;
+int[,] GenerateSpyralArray(int m, int n){
+    int[,] array = new int[m, n];
+    array[0,0] = 0;
+    int sum = -1;
+    for (int i = 0; i < array.GetLength(0); i++) {
+        for (int j = 0; j < array.GetLength(1); j++) {
+            array[i,j] = sum + 1; 
+            sum++;
+            if (i == array.GetLength(0)- 1){
+                
+
+            }
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array){
+    for (int i = 0; i < array.GetLength(0); i++) {
+        for (int j = 0; j < array.GetLength(1); j++) {
+            Console.Write($"{array[i,j]}  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+var array = GenerateSpyralArray(m, n);
+Console.WriteLine("Сформирован спиральный массив: ");
+PrintArray(array);

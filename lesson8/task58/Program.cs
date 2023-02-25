923059d0ad58e69d08755bd0e9bb195374e4944e
KOLOK777 <kolevg3@gmail.com>
2023-02-25 17:17:22 +0300

    исправлено после замечания преподавателя

--- lesson8/task58/Program.cs
+++ lesson8/task58/Program.cs
@@ -6,14 +6,22 @@
 // 18 20
 // 15 18
 
-Console.WriteLine("Введите число строк массивов (целое положительное число) 'm': "); 
+Console.WriteLine("Введите число строк массива1 (целое положительное число) 'm': "); 
 int m; 
 while (!Int32.TryParse(Console.ReadLine(), out m) || m <= 0)
-    Console.WriteLine("Введены некорретные данные. Введите число строк массивов (целое положительное число) 'm': ");
-Console.WriteLine("Введите число столбцов массивов (целое положительное число) 'n': "); 
+    Console.WriteLine("Введены некорретные данные. Введите число строк массива1 (целое положительное число) 'm': ");
+Console.WriteLine("Введите число столбцов массива1 (целое положительное число) 'n': "); 
 int n; 
 while (!Int32.TryParse(Console.ReadLine(), out n) || n <= 0)
-    Console.WriteLine("Введены некорретные данные. Введите число столбцов массивов (целое положительное число) 'n': ");
+    Console.WriteLine("Введены некорретные данные. Введите число столбцов массива1 (целое положительное число) 'n': ");
+Console.WriteLine("Введите число строк массива2 (целое положительное число) 'p': "); 
+int p; 
+while (!Int32.TryParse(Console.ReadLine(), out p) || p <= 0)
+    Console.WriteLine("Введены некорретные данные. Введите число строк массива2 (целое положительное число) 'p': ");
+Console.WriteLine("Введите число столбцов массива2 (целое положительное число) 'q': "); 
+int q; 
+while (!Int32.TryParse(Console.ReadLine(), out q) || q <= 0)
+    Console.WriteLine("Введены некорретные данные. Введите число столбцов массива2 (целое положительное число) 'q': ");
 
 int[,] GenerateArray(int m, int n){
     int[,] array = new int[m, n];
@@ -46,20 +54,36 @@ void PrintDoubleArray(double[,] array){
 
 double[,] MultiplyArray(int[,] array, int[,] array2){
     double[,] arrayResult = new double[m,n];
-    for (int i = 0; i < array2.GetLength(0); i++) {
-        for (int j = 0; j < array2.GetLength(1); j++) {
-            arrayResult[i,j] = array[i,j] * array2[i,j];
+    if (array.GetLength(0) == array2.GetLength(0) & array.GetLength(1) == array2.GetLength(1)) {
+        for (int i = 0; i < array2.GetLength(0); i++) {
+            for (int j = 0; j < array2.GetLength(1); j++) {
+                arrayResult[i,j] = array[i,j] * array2[i,j];
             }
         }
-    return arrayResult;
-    }
+            return arrayResult;
+        }
+    else if (array.GetLength(0) == array2.GetLength(1) & array.GetLength(1) == array2.GetLength(0)) {
+        for (int i = 0; i < array2.GetLength(1); i++) {
+            for (int j = 0; j < array2.GetLength(0); j++) {
+                arrayResult[i,j] = array[i,j] * array2[j,i];
+            }
+        }
+        return arrayResult;
+        }
+        else 
+            return arrayResult;
+        }
 
 var array = GenerateArray(m, n);
 Console.WriteLine("Сформирован массив1: ");
 PrintArray(array);
-var array2 = GenerateArray(m, n);
+var array2 = GenerateArray(p, q);
 Console.WriteLine("Сформирован массив2: ");
 PrintArray(array2);
-var arrayResult = MultiplyArray(array, array2);
-Console.WriteLine("Массивы перемножены: ");
-PrintDoubleArray(arrayResult);
+if (m + n == p + q) {
+    var arrayResult = MultiplyArray(array, array2);
+    Console.WriteLine("Массивы перемножены: ");
+    PrintDoubleArray(arrayResult);
+    }
+else
+    Console.WriteLine("Массивы не совпадают !");
